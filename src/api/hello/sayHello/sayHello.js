796fb6ecb6d6056a74dc23e73fc7abb6f39b7ea2
wwwkang8 <wwwkang8@gmail.com>
2020-08-17 20:38:08 +0900

    Remove ignore files from remote repository

--- src/api/hello/sayHello/sayHello.js
+++ src/api/hello/sayHello/sayHello.js
@@ -1,5 +1,11 @@
+import { prisma } from "../../../../generated/prisma-client"
+
 export default{
     Query:{
-        sayHello: () => "Hello"
+        sayHello: async() => {
+            console.log(await prisma.users());
+
+            return "HELLO"
+        }
     }
 };
