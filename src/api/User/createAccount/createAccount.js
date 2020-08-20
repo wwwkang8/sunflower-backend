2f2c41e124d6234a9cf0993f8044b0d7a362943c
wwwkang8 <wwwkang8@gmail.com>
2020-08-21 00:11:32 +0900

    Solve Graphql syntax error

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -8,7 +8,7 @@ export default {
         createAccount: async(_, args) => {
             // args는 userName, email, firstName, lastName, bio 매개변수로 가지고 있다.
             // args의 매개변수 값을 각각의 변수에 할당.
-            const { userName, email, firstName="", lastName = "", bio = "" } = args;
+            const { userName, email, firstName= "", lastName = "", bio = "" } = args;
 
             // prisma client에 내장된 createUser를 사용하여 User를 생성한다.
             // 생성한 User를 화면에 반환.
