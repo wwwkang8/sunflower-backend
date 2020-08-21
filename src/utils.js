f96195b3b4a4667ec7cfa1c587549c3df5772516
wwwkang8 <wwwkang8@gmail.com>
2020-08-21 23:47:06 +0900

    #3.4 sendMail function with Nodemailer

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,41 @@
 import { adjectives, nouns } from "./words";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({path: path.resolve(__dirname, ".env") });
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+
 
 export const generateSecret = () => {
 
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 
+};
+
+console.log("유저이름 : " + process.env.SENDGRID_USERNAME);
+
+export const sendMail = (email) => {
+    
+    const options = {
+        auth: {
+            api_user: "jeongho0812@gmail.com",
+            api_key: ""
+        }
+    };
+
+    const client = nodemailer.createTransport(sgTransport(options));
+    return client.sendMail(email);
+};
+
+
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from: "jeongho0812@gmail.com",
+        to: address,
+        subject: "Login Secret for Prismagram",
+        html:  `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to login`
+    }
+
+    return sendMail(email);
 }
