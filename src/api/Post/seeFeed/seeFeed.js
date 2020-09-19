4f401118e010e6438abd548f2ce905ca1409c616
wwwkang8 <wwwkang8@gmail.com>
2020-09-19 13:48:08 +0900

    seeFeed Resolver review

---
+++ src/api/Post/seeFeed/seeFeed.js
@@ -0,0 +1,21 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        seeFeed: async (parent, args, {request, isAuthenticated}, info) => {
+            isAuthenticated(request);
+
+            const { user } = request;
+            const following = await prisma.user({id: user.id}).following();
+            console.log(following);
+
+            return prisma.posts({
+                where: {
+                    user: {
+                        id_in: [...following.map(user => user.id), user.id]
+                    }
+                }
+            });
+        }
+    }
+}
